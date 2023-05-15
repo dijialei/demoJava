98c9d0a30c31eebe7318641141cc5a3d9dee9cbc
dijialei <dijialei0605@gmail.com>
2023-05-15 13:12:07 +0200

    DECLAREZLES VARIABLES

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,23 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+    public static void main(String[] args) {
+        byte varByte = 0;
+        int varInt = 0;
+        short varShort = 0;
+        long varLong = 0;
+        float varFloat = 0f;
+        double varDouble = 0;
+        char varChar = 0;
+        boolean varBoolean = true;
+        String varString = "0";
+        System.out.println("directementun littéral:" + 0);
+        System.out.println("une variable:" + varByte);
+        String randomString= "Voici le résultat d’un calcul : 1+5=6";
+        System.out.println("afficher randomString:"+ randomString);
+        randomString= "Voici le résultat d’un calcul :\n 1+5=6";
+
+
+    }
+}
