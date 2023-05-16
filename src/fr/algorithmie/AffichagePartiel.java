9e072ae9843c99ce5e95070642d0127027cf774e
dijialei <dijialei0605@gmail.com>
2023-05-16 11:15:38 +0200

    AFFICHAGE PARTIEL

---
+++ src/fr/algorithmie/AffichagePartiel.java
@@ -0,0 +1,25 @@
+package fr.algorithmie;
+
+public class AffichagePartiel {
+    public static void main(String[] args) {
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+        int lengthArr = array.length;
+        for (int i = 0; i < lengthArr; i++) {
+            if (array[i] > 3) System.out.println(array[i]);
+        }
+        System.out.println("*************************");
+        for (int i = 0; i < lengthArr; i++) {
+            if (array[i] % 2 == 0) System.out.println(array[i]);
+        }
+        System.out.println("*************************");
+        for (int i = 0; i < lengthArr; i++) {
+            if (i != 0 && i % 2 == 0) System.out.println(array[i]);
+        }
+        System.out.println("*************************");
+        for (int i = 0; i < lengthArr; i++) {
+            if ( i % 2 == 1) System.out.println(array[i]);
+        }
+        System.out.println("*************************");
+
+    }
+}
